50c76a4c92e25e805579999c87036ac87cb850d5
bany8 <bart-7@outlook.com>
2026-02-15 14:31:05 +0100

    First steps

---
+++ polisticks/src/HelloPlugin.rs
@@ -0,0 +1,25 @@
+fn hello_world() {
+   println!("hello world");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don't need to change any other names.
+        }
+    }
+}
+
