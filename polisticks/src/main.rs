e2332b15ad10b40fc4408ce35100fddc0ab87b5b
bany8 <bart-7@outlook.com>
2026-03-01 16:24:45 +0100

    build my own verion what i learned (ongoing)

--- polisticks/src/main.rs
+++ polisticks/src/main.rs
@@ -1,51 +1,29 @@
 use bevy::prelude::*;
 
 fn main() {
-   App::new()
-       .add_plugins(DefaultPlugins)
-       .add_plugins(HelloPlugin)
-       .run();
+    App::new()
+            .add_plugins(DefaultPlugins)
+            .add_plugins(Cars)
+            .run();
 }
 
 #[derive(Component)]
-struct Person;
+struct Car;
 
 #[derive(Component)]
-struct Name(String);
+struct Condition(bool);
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+#[derive(Component)]
+struct Condition(String);
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+fn add_cars(mut commands: Commands) {
+    commands.spawn((Car, Condition));
 }
 
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
+pub struct Cars;
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
+impl Plugin for Cars {
+    fn build(&self, app: &mut App){
+        app.add_systems(Startup, add_cars);
     }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
-    }
-    
 }
