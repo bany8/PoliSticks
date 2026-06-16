7b47b70622a229b36ef2eed70b99d52c302ecdac
Bartosz Nysztal <BNysztal@elmech.pl>
2026-06-16 11:28:11 +0200

    starting

--- polisticks/src/main.rs
+++ polisticks/src/main.rs
@@ -1,6 +1,21 @@
-use bevy::prelude::*;
+use std::io;
+
+struct Player{
+    name: String,
+    symbol: char,
+}
 
 fn main() {
-    App::new()
+    let mut player_1: Player = Player("Gracz1".to_string(), X);
+    let mut player_2: Player;
+
+    init_game(&mut player_1, &mut player_2);
+
+    
+    println!("co tam ?")
+
+}
+
+fn init_game(p1: &mut Player, p2: &mut Player) {
 
 }
