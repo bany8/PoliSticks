6049dc0163155587c09e797bd2664ee14a57c94f
Bartosz Nysztal <BNysztal@elmech.pl>
2026-06-18 16:05:00 +0200

    going forward

--- polisticks/src/main.rs
+++ polisticks/src/main.rs
@@ -1,22 +1,11 @@
+struct Player{
+    name: String,
+    symbol: char,
+}
+
 fn main() {
-    struct Player{
-        name: String,
-        symbol: char,
-    }
-
-    impl Player {
-        fn new(name: String, symbol: char) -> Player{
-            Self {name: name, 
-                symbol: symbol};
-        }
-
-        fn change_name(&mut self, name: String){
-            self.name = name;
-        }
-    
-    }
-    let mut player_1: Player = Player.new("Player1".to_string(), 'X');
-    let mut player_2: Player = Player.new("Player2".to_string(), 'O');
+    let mut player_1: Player = Player {name: "Player1".to_string(), symbol: 'X'};
+    let mut player_2: Player = Player {name: "Player2".to_string(), symbol: 'O'};
 
     println!("co tam ?")
 }
