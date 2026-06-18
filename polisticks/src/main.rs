080403b50af30308851447ace37b64631f01a16d
Bartosz Nysztal <BNysztal@elmech.pl>
2026-06-18 15:59:49 +0200

    trying something but its hard for now

--- polisticks/src/main.rs
+++ polisticks/src/main.rs
@@ -1,21 +1,27 @@
-use std::io;
-
-struct Player{
-    name: String,
-    symbol: char,
-}
-
 fn main() {
-    let mut player_1: Player = Player("Gracz1".to_string(), X);
-    let mut player_2: Player;
+    struct Player{
+        name: String,
+        symbol: char,
+    }
 
-    init_game(&mut player_1, &mut player_2);
+    impl Player {
+        fn new(name: String, symbol: char) -> Player{
+            Self {name: name, 
+                symbol: symbol};
+        }
 
+        fn change_name(&mut self, name: String){
+            self.name = name;
+        }
     
-    println!("co tam ?")
+    }
+    let mut player_1: Player = Player.new("Player1".to_string(), 'X');
+    let mut player_2: Player = Player.new("Player2".to_string(), 'O');
 
+    println!("co tam ?")
 }
 
-fn init_game(p1: &mut Player, p2: &mut Player) {
-
+fn menu(){
+    println!("Witamy w Polisticks !!");
+    println!("Wybierz opcję");
 }
